fb4089ef995026864bcf0fd6fa1abd21a54b909e
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-26 22:01:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	public:
+	width;
+	height;
+	
+	Rectangle(width,height){
+		this.width = width;
+	 this.height = height;
+	}
+  get width(){
+	  return width;
+  }
+ get height(){
+	 return height;
+ }
+ getArea(){
+	 return width*height;
+ }
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+   public:
+	side;
+	super(width,height);
+	Square(side){
+		this.side = side;
+	}
+	getPerimeter(){
+		return  4*side;
+	}
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
